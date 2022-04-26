4112f89dade5d36631a4d3cb215490806ec1cfca
Veerababu T <t.veerababu32@gmail.com>
2022-04-26 16:44:13 +0530

    added on scroll animation

--- main.js
+++ main.js
@@ -5,3 +5,21 @@ hamburgerBtn.onclick = function(){
     hamburgerBtn.classList.toggle('cross');
     collapseMenu.classList.toggle('show');
 }
+
+window.addEventListener('scroll', reveal);
+
+function reveal(){
+    var reveals = document.querySelectorAll('.reveal');
+    for(var i = 0; i < reveals.length; i++){
+        var windowHeight = window.innerHeight;
+        var revealTop = reveals[i].getBoundingClientRect().top;
+        var revealPoint = 150;
+
+        if(revealTop < windowHeight - revealPoint){
+            reveals[i].classList.add('active');
+        }
+        else{
+            reveals[i].classList.remove('active');
+        }
+    }
+}
